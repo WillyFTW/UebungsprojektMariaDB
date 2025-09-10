bd650bac2a15801cebd4a23c80bbd7a4abc358a8
Willy <willyca97@gmail.com>
2025-09-10 13:32:52 +0200

    SkySQL connection implemented and added to .gitignore

--- db.js
+++ db.js
@@ -1,12 +1,24 @@
 const mariadb = require("mariadb");
+require("dotenv").config(); // Load environment variables from .env file
 
-// Create a connection pool
-const pool = mariadb.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Wilhelm",
-  database: "scriptsdb",
-  connectionLimit: 5,
-});
+// Dynamically create the pool based on the environment
+const pool = mariadb.createPool(
+  process.env.NODE_ENV === "production"
+    ? {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT, 10) || 5, // Default to 5 if not set
+        rejectUnauthorized: true, // Ensure SSL certificate is verified
+      }
+    : {
+        host: "localhost",
+        user: "root",
+        password: "Wilhelm",
+        database: "scriptsdb",
+        connectionLimit: 5,
+      }
+);
 
 module.exports.pool = pool;
